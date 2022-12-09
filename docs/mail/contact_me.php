894909a15777b13b5af147729386811c3ed3fe07
chuckthepiranha <35348129+chuckthepiranha@users.noreply.github.com>
2022-12-08 20:35:51 -0500

    make anonymous messages allowed

--- docs/mail/contact_me.php
+++ docs/mail/contact_me.php
@@ -2,10 +2,7 @@
    // Check for empty fields
    if (
       empty($_POST['name'])  		||
-      empty($_POST['email']) 		||
-      empty($_POST['phone']) 		||
       empty($_POST['message'])	||
-      !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)
    ) {
       echo "No arguments Provided!";
 
@@ -13,9 +10,17 @@
    }
 
    $name = $_POST['name'];
-   $email_address = $_POST['email'];
-   $phone = $_POST['phone'];
+   $email_address = $_POST['email'] ?? "";
+   $phone = $_POST['phone'] ?? "";
    $message = $_POST['message'];
+
+   if ($email) {
+      if (!filter_var($email,FILTER_VALIDATE_EMAIL) {
+         echo "Invalid email";
+         
+         return false;
+      }
+   }
 
    // Create the email and send the message
    $to = 'kastner@mit.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
